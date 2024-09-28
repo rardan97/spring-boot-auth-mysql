337e67d3a813938a77ee45f76e75e97018f3b776
rname97 <risalardan@gmail.com>
2024-09-28 18:32:10 +0700

    update project

--- src/main/java/com/blackcode/spring_boot_auth_mysql/controller/HomeController.java
+++ src/main/java/com/blackcode/spring_boot_auth_mysql/controller/HomeController.java
@@ -1,12 +1,10 @@
 package com.blackcode.spring_boot_auth_mysql.controller;
 
-
-import com.blackcode.spring_boot_auth_mysql.model.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/home")
